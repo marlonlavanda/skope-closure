20717950d87dbaf017c18c644bd75c3528391951
Marlon Lavanda <marlonlavanda@hotmail.com>
2020-12-18 23:23:15 +0000

    diferentes maneras de declarar una variable

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,3 +1,27 @@
-var hello = "Confirma mijin";
+var hello = "Confirma ";
+let world = "mijin";
+const helloWorld = "Confirma mijin";
 
-console.log(hello);
+// console.log(hello);
+
+// const anotherFunction = () => {
+//   console.log(hello);
+//   console.log(world);
+//   console.log(helloWorld);
+// };
+
+// anotherFunction();
+
+const helloWorld = () => {
+  globalVar = "I am global";
+};
+
+helloWorld();
+console.log(globalVar);
+
+const anotherFunction = () => {
+  var localVar = (globalVar = "I am Global");
+};
+
+anotherFunction();
+console.log(globalVar);
